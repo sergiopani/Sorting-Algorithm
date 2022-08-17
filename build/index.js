50b16471648b718f56670dd418b4c41f79ddde45
sergiopani <xpanisergio@gmail.com>
2022-08-17 11:38:24 +0200

    typeof added

--- build/index.js
+++ build/index.js
@@ -1,6 +1,26 @@
 "use strict";
-console.log('Hi there bitch');
-const logSomething = () => {
-    console.log("This is something!");
-};
-logSomething();
+class Sorter {
+    //Take a collection of data and sort all elements
+    constructor(collection) {
+        this.collection = collection;
+    }
+    sort() {
+        //Implementation of buble sort
+        //This is the same as this.collection.length
+        const { length } = this.collection;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    //We need to do the swap
+                    const leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = this.collection[leftHand];
+                }
+            }
+        }
+    }
+}
+const sorter = new Sorter([2, 4, 6, 8, 3, 4]);
+console.log(sorter.collection);
+sorter.sort();
+console.log(sorter.collection);
